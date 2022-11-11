3939023837904ca079d6e6bc300a0b8e5b788b81
Tóth Márton <tmarcidev@gmail.com>
2022-11-11 16:52:17 +0100

    label creation is donw

--- app/Http/Controllers/LabelController.php
+++ app/Http/Controllers/LabelController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Label;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Validation\Rule;
 
 
 class LabelController extends Controller
@@ -11,8 +14,31 @@ class LabelController extends Controller
     public function create()
     {
         $this->authorize('create', App\Label::class);
-        return view("labels.create", [
-            //"labels" => Label::all(),
+        return view("labels.create");
+    }
+
+    public function store(Request $request)
+    {
+        $this->authorize('create', App\Label::class);
+
+        if(is_null($request["display"])) {
+            $request["display"] = false;
+        }
+        $validated = $request->validate([
+            "name" => "required",
+            "color" => [
+                'required',
+                'regex:/^#([a-f0-9]{6}|[a-f0-9]{3})$/i'
+            ],
+            "display" => "required|bool",
         ]);
+
+        // Ide már csak akkor jut el a vezérlés, hogyha a validáció sikeres volt
+        Label::factory()->create($validated);
+
+        Session::flash("label_created", $validated["name"]);
+
+        // return redirect()->route('categories.create');
+        return Redirect::route("labels.create");
     }
 }
